d020536fd6785865a4acc9f5d527a39031b7d0ee
Aaron Silinskas <aaron@mindwidgets.com>
2019-02-03 11:39:37 -0800

    simply button state to capture all state in a single value

--- examples/buttonlock.cpp
+++ examples/buttonlock.cpp
@@ -2,22 +2,20 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
-const uint32_t BUTTON_PINS[] = {A2, A3, A4, A5};
+const uint32_t BUTTON_PINS[] = {A1, A2, A3, A4, A5};
 const uint8_t BUTTON_COUNT = sizeof(BUTTON_PINS) / sizeof(BUTTON_PINS[0]);
 
 const uint8_t PASSWORD_LENGTH = 4;
 uint8_t password[PASSWORD_LENGTH];
 uint8_t passwordPosition;
 
-bool buttonPressed[BUTTON_COUNT];
-
 enum GameState
 {
     WaitingForPlayer,
     ButtonPressed,
-    ButtonReleased,
     CorrectButton,
     IncorrectButton,
+    WaitingForRelease,
     Unlocked
 };
 GameState gameState;
@@ -30,7 +28,6 @@ void setupButtons()
     for (uint8_t i = 0; i < BUTTON_COUNT; i++)
     {
         pinMode(BUTTON_PINS[i], INPUT_PULLUP);
-        buttonPressed[i] = false;
     }
 }
 
@@ -41,7 +38,7 @@ void setupPassword()
     Serial.print(F("Generating password:"));
     for (uint8_t i = 0; i < PASSWORD_LENGTH; i++)
     {
-        password[i] = random(BUTTON_COUNT);
+        password[i] = (1 << random(BUTTON_COUNT));
         Serial.print(F(" "));
         Serial.print(password[i]);
     }
@@ -77,78 +74,21 @@ void updateGameState(GameState newState)
     gameState = newState;
 }
 
-bool updateButtonTracking()
+uint16_t readButtonState()
 {
-    bool buttonsChanged = false;
+    uint16_t buttonState = 0;
     for (uint8_t i = 0; i < BUTTON_COUNT; i++)
     {
+        buttonState <<= 1;
         if (digitalRead(BUTTON_PINS[i]) == 0)
-        {
-            buttonPressed[i] |= true;
-            buttonsChanged = true;
-        }
-    }
-    return buttonsChanged;
-}
-
-void resetButtonTracking()
-{
-    for (uint8_t i = 0; i < BUTTON_COUNT; i++)
-    {
-        buttonPressed[i] = false;
-    }
-}
-
-bool anyButtonPressed()
-{
-    for (uint8_t i = 0; i < BUTTON_COUNT; i++)
-    {
-        if (buttonPressed[i])
-        {
-            return true;
-        }
-    }
-    return false;
-}
-
-bool allButtonsReleased()
-{
-    for (uint8_t i = 0; i < BUTTON_COUNT; i++)
-    {
-        if (digitalRead(BUTTON_PINS[i]) == 0)
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-void checkButtonAgainstPassword()
-{
-    uint32_t correctButton = password[passwordPosition];
-    bool incorrectButtonPressed = false;
-    bool correctButtonPressed = false;
-
-    for (uint8_t button = 0; button < BUTTON_COUNT; button++)
-    {
-        if (buttonPressed[button])
         {
             Serial.print(F("Pressed: "));
-            Serial.println(button);
+            Serial.println(BUTTON_PINS[i]);
 
-            correctButtonPressed = (button == correctButton);
-            incorrectButtonPressed |= (button != correctButton);
+            buttonState |= 1;
         }
     }
-
-    if (correctButtonPressed && !incorrectButtonPressed)
-    {
-        updateGameState(CorrectButton);
-    }
-    else
-    {
-        updateGameState(IncorrectButton);
-    }
+    return buttonState;
 }
 
 void fadeInPixels(uint64_t transitionTime, uint16_t firstPixel, uint16_t pixelCount, uint8_t rT, uint8_t gT, uint8_t bT)
@@ -208,31 +148,30 @@ void loop()
     switch (gameState)
     {
     case WaitingForPlayer:
-        updateButtonTracking();
-        if (anyButtonPressed())
+        if (readButtonState() != 0)
         {
             updateGameState(ButtonPressed);
         }
+
         break;
     case ButtonPressed:
-        if (allButtonsReleased())
+        if (password[passwordPosition] == readButtonState())
         {
-            updateGameState(ButtonReleased);
+            updateGameState(CorrectButton);
+        }
+        else
+        {
+            updateGameState(IncorrectButton);
         }
         break;
-    case ButtonReleased:
-        checkButtonAgainstPassword();
-        break;
     case CorrectButton:
-        Serial.println("Yay!");
-
-        resetButtonTracking();
+        Serial.println("Correct Button!");
 
         passwordPosition++;
         updateLightsForProgress();
         if (passwordPosition < PASSWORD_LENGTH)
         {
-            updateGameState(WaitingForPlayer);
+            updateGameState(WaitingForRelease);
         }
         else
         {
@@ -241,15 +180,19 @@ void loop()
 
         break;
     case IncorrectButton:
-        Serial.println("Boo!");
-
-        resetButtonTracking();
+        Serial.println("Incorrect Button!");
 
         passwordPosition = 0;
 
         updateLightsForReset();
 
-        updateGameState(WaitingForPlayer);
+        updateGameState(WaitingForRelease);
+        break;
+    case WaitingForRelease:
+        if (readButtonState() == 0)
+        {
+            updateGameState(WaitingForPlayer);
+        }
         break;
     case Unlocked:
         Serial.println("UNLOCKED!");
