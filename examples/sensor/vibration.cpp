cba28ab036ddff8db9b0afd696f12a77bf35fce7
Aaron Silinskas <aaron@mindwidgets.com>
2019-04-28 10:23:36 -0700

    vibration prototype uses 3 LED indicators that light for 1 second when sensor is triggered

--- examples/sensor/vibration.cpp
+++ examples/sensor/vibration.cpp
@@ -1,39 +1,77 @@
 #include <Arduino.h>
 
-const int ledPin = 13;
-const int PIN_SLOW_VIBRATION = 12;
-const int PIN_MEDIUM_VIBRATION = 11;
-const int PIN_FAST_VIBRATION = 10;
+const uint32_t ledPin = 13;
 
-int ledState = LOW;
+uint32_t vibrationCount = 0;
+uint32_t lastTimeMs;
 
-void setup() {
-  Serial.begin(9600);
+struct VibrationSensor
+{
+    char *name;
+    uint32_t sensorPin;
+    uint32_t ledPin;
+    uint32_t timer;
+};
+const uint8_t SENSOR_COUNT = 3;
+VibrationSensor sensors[SENSOR_COUNT];
 
-  pinMode(ledPin, OUTPUT);
-  pinMode(PIN_SLOW_VIBRATION, INPUT_PULLUP);
-  pinMode(PIN_MEDIUM_VIBRATION, INPUT_PULLUP);
-  pinMode(PIN_FAST_VIBRATION, INPUT_PULLUP);
+void setup()
+{
+    Serial.begin(9600);
+
+    sensors[0].name = "Slow";
+    sensors[0].sensorPin = 12;
+    sensors[0].ledPin = A1;
+    sensors[0].timer = 0;
+    sensors[1].name = "Medium";
+    sensors[1].sensorPin = 11;
+    sensors[1].ledPin = A2;
+    sensors[1].timer = 0;
+    sensors[2].name = "Fast";
+    sensors[2].sensorPin = 10;
+    sensors[2].ledPin = A3;
+    sensors[2].timer = 0;
+
+    for (uint8_t i = 0; i < SENSOR_COUNT; i++)
+    {
+        pinMode(sensors[i].sensorPin, INPUT_PULLUP);
+        pinMode(sensors[i].ledPin, OUTPUT);
+    }
+
+    lastTimeMs = millis();
 }
 
-void loop() {
-  // read the vibration sensors
-  bool slowValue = digitalRead(PIN_SLOW_VIBRATION) == 0;
-  bool mediumValue = digitalRead(PIN_MEDIUM_VIBRATION) == 0;
-  bool fastValue = digitalRead(PIN_FAST_VIBRATION) == 0;
+void loop()
+{
+    // update ellapsed time
+    uint32_t currentTimeMs = millis();
+    uint32_t ellapsedTimeMs = currentTimeMs - lastTimeMs;
+    lastTimeMs = currentTimeMs;
 
-  // if any vibration triggers happened, output which ones
-  if (slowValue || mediumValue || fastValue) {
-      Serial.print("Vibration! ");
-      Serial.print(slowValue);
-      Serial.print(" ");
-      Serial.print(mediumValue);
-      Serial.print(" ");
-      Serial.println(fastValue);
+    // update each sensor state
+    for (uint8_t i = 0; i < SENSOR_COUNT; i++)
+    {
+        uint32_t timer = sensors[i].timer;
+        if (timer > 0)
+        {
+            if (ellapsedTimeMs >= timer)
+            {
+                sensors[i].timer = 0;
+                digitalWrite(sensors[i].ledPin, 0);
+            }
+            else
+            {
+                sensors[i].timer = timer - ellapsedTimeMs;
+            }
+        }
+        else if (digitalRead(sensors[i].sensorPin) == 0)
+        {
+            sensors[i].timer = 1000;
+            digitalWrite(sensors[i].ledPin, 1);
+            Serial.print("Vibration detected on: ");
+            Serial.println(sensors[i].name);
+        }
+    }
 
-      ledState = !ledState;
-      digitalWrite(ledPin, ledState);
-  }
-
-  delay(100);
+    delay(10);
 }
